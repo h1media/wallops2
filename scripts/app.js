f75c8ef5fdf895634b7d42a586eb9907b4e59698
mkurdybanowski <mkurdybanowski@3a.pl>
2018-02-26 12:07:20 +0100

    Smooth scrolling domain fix

--- scripts/app.js
+++ scripts/app.js
@@ -47,4 +47,16 @@
         });
     });
 
+    console.log("Output;");
+    console.log(location.hostname);
+    console.log(document.domain);
+    alert(window.location.hostname)
+
+    console.log("document.URL : "+document.URL);
+    console.log("document.location.href : "+document.location.href);
+    console.log("document.location.origin : "+document.location.origin);
+    console.log("document.location.hostname : "+document.location.hostname);
+    console.log("document.location.host : "+document.location.host);
+    console.log("document.location.pathname : "+document.location.pathname);
+
 })(jQuery);
